78a2916566465baff19f4919d00345a812893809
Ben Kelly <bk@ancilla.ca>
2014-04-23 13:58:37 -0400

    replace profile.clj with basic application template

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
 (defproject spellcast "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5"]
-                 [lib-noir "0.4.6"]]
-  :plugins [[lein-ring "0.8.2"]]
-  :ring {:handler spellcast.handler/app}
-  :main spellcast.core
+  :description "A Clojure implementation of the Bartle/Plotkin classic"
+  :url "https://github.com/toxicfrog/spellcast"
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main ^:skip-aot spellcast.core
+  ; handle multiple UIs by defining a different profile for each one, each
+  ; with its own :main and :uberjar-name, and then
+  ; $ lein with-profile tty,gui,http uberjar
   :profiles
-  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
+  {:uberjar {:aot :all}})
