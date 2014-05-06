71230f8b7c8407875049e94d1f16d35ce2dcf11c
Ben Kelly <bk@ancilla.ca>
2014-05-05 22:10:52 -0400

    Bind *game-out* and *game-in*; use for send-to
    I don't think this API is fully baked yet, but it's ok for now.

--- src/spellcast/game.clj
+++ src/spellcast/game.clj
@@ -4,7 +4,8 @@
          '[spellcast.util :refer :all]
          '[taoensso.timbre :as log])
 
-(def ^:dynamic *game* nil)
+(def ^:dynamic *game-out* nil)
+(def ^:dynamic *game-in* nil)
 
 (defmacro defstate [name & fns]
   (let [unknown (fn [game msg]
@@ -58,10 +59,9 @@
 (defn new-player [name]
   {:name name :left '() :right '()})
 
-(defn- send-to [game id msg]
-  (log/debug (:out game) id msg)
-  (>!! (:out game) (with-meta msg {:id id}))
-  game)
+(defn- send-to [id msg]
+  (log/debug *game-out* id msg)
+  (>!! *game-out* (with-meta msg {:id id})))
 
 (defn- remove-player [game player]
   (update-in game [:players]
@@ -71,8 +71,8 @@
 
 (defn- disconnect [game user msg]
   (log/infof "Disconnecting user %d: %s" user msg)
-  (send-to game user (list :error msg))
-  (send-to game user '(:close))
+  (send-to user (list :error msg))
+  (send-to user '(:close))
   (remove-player game user))
 
 (defn- get-player [game key]
@@ -85,7 +85,7 @@
   ; game at player limit
   ; password required and wrong/no password provided
   (cond
-    (get-player game id) (send-to game id (list :error "You are already logged in."))
+    (get-player game id) (do (send-to id (list :error "You are already logged in.")) game)
     (get-player game name) (disconnect game id "A player with that name already exists.")
     (>= (count (:players game))
         (:max-players game)) (disconnect game id "Player limit reached.")
@@ -93,7 +93,7 @@
          (not= nil (:pass game))) (disconnect game id "Password incorrect.")
     :else (let [player {:name name :id id}]
             (log/info "Player" id "logged in as" name)
-            (send-to game :all (list :info (str name " has joined the game.")))
+            (send-to :all (list :info (str name " has joined the game.")))
             (-> game
                 (assoc-in [:players id] player)))))
 
@@ -111,31 +111,27 @@
   (defn :login [game id name]
     (if (string? name)
       (add-client game id name)
-      (send-to game id (list :error "Malformed login request."))))
+      (do (send-to id (list :error "Malformed login request.")) game)))
   (defn :ready [game id ready]
     (if (get-player game id)
-      (do (send-to game :all (list :info (str id " ready: " ready)))
+      (do (send-to :all (list :info (str id " ready: " ready)))
         (assoc-in game [:players id :ready] ready))
-      (send-to game id (list :error "You are not logged in."))))
+      (do (send-to id (list :error "You are not logged in.")) game)))
   (defn :disconnect [game id]
     (remove-player game id)))
-
-(defn- with-game [game f & args]
-  (binding [*game* game]
-    (apply f game args)))
 
 (defn run-state [game state]
   (log/debug "State runner init" game state)
   (let [in (:in game)
         {:keys [begin end done? handlers default]} state]
-    (loop [game (with-game game begin)]
+    (loop [game (begin game)]
       (log/debug "state iteration game=" game)
-      (if (with-game game done?)
-        (with-game game end)
+      (if (done? game)
+        (end game)
         (let [msg (<!! in)
               handler (get handlers (keyword (first msg)) default)]
           (log/debug "run-state handling message" (meta msg) msg)
-          (recur (apply with-game game handler (get-meta msg :id) (rest msg))))))))
+          (recur (apply handler game (get-meta msg :id) (rest msg))))))))
 
 (defn- init-game
   "Perform game startup tasks like collecting players."
@@ -146,16 +142,18 @@
 (defn- report-end [game]
   (log/info "Game over!")
   (log/debug "Final game state:" game)
-  (send-to game :all (list :info "Finished!"))
-  (send-to game :all '(:close))
+  (send-to :all (list :info "Finished!"))
+  (send-to :all '(:close))
   (close! (:in game))
   game)
 
 (defn run-game [game]
-  (loop [game (init-game game)]
-    (if (game-finished? game)
-      (report-end game)
-      (recur (run-turn game)))))
+  (binding [*game-out* (:out game)
+            *game-in* (:in game)]
+    (loop [game (init-game game)]
+      (if (game-finished? game)
+        (report-end game)
+        (recur (run-turn game))))))
 
 (defn new-game
   "Return a new Spellcast game state."
